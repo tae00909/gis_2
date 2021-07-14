f4bc3e85adbfd0998841149158522535d749bc79
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-14 14:56:51 +0900

    7/14 3ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 #실제 주소를 안적어도 accountapp으로 바로 라우팅 하기 위해 썼음
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import NewModel
 
 
@@ -14,8 +19,17 @@ def hello_world(request):
         model_instance.text = temp
         model_instance.save()
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'model_instance':model_instance})
+        data_list = NewModel.objects.all()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'GET METHOD!'})
+                      context={'data_list': data_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp/hello_world')
+    template_name = 'accountapp/create.html'
+
