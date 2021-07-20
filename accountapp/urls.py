5dfae9e50df6e205319fe74e8753acdf24dcaa9d
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-20 09:27:31 +0900

    7/20 3ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailVeiw
 
 #실제 주소를 안적어도 accountapp으로 바로 라우팅 하기 위해 썼음
 app_name = 'accountapp'
@@ -15,5 +15,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
 
-    path('create/', AccountCreateView.as_view(), name='create')
+    path('create/', AccountCreateView.as_view(), name='create'),
+
+    path('detail/<int:pk>', AccountDetailVeiw.as_view(), name='detail')
 ]
